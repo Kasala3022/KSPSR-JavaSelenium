8cf3f72d84d52999a1b002cf32428e1bfbc642ff
kasala3022 <kasalasrinivasreddy@gmail.com>
2025-09-06 19:46:19 +0530

    Even numbers

---
+++ src/spj/EvenNumbers.java
@@ -0,0 +1,19 @@
+package spj;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=7;
+		if(i%2==0) {
+			System.out.println("Print even numbers: "+i);
+			
+		}
+	else {
+		System.out.println("print odd: "+i);
+		
+
+	}
+
+}
+}
